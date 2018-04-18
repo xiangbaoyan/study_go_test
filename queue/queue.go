d21351dd5162012a226ea34ccdf27a17f70861cc
xiangbaoyan <271190187@qq.com>
2018-04-18 21:27:19 +0800

    Merge branch 'master' of https://github.com/xiangbaoyan/study_go_test
    # Conflicts:
    #	.gitignore

---
+++ queue/queue.go
@@ -0,0 +1,17 @@
+package queue
+
+type Queue []int
+
+func (q *Queue) Pop() int {
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head
+}
+
+func (q *Queue) Push(a int) {
+	*q = append(*q, a)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return 0 == len(*q)
+}
