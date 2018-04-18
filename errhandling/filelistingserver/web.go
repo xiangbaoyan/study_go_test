9ecf5b63e0d8a4e9f5ae84de680c764e69a1edb1
xiangbaoyan <271190187@qq.com>
2018-04-18 11:17:16 +0800

    Merge branch 'master' of https://github.com/xiangbaoyan/study_go_test
    # Conflicts:
    #	.gitignore

--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"io/ioutil"
+	"github.com/xiangbaoyan/study_go_test/functional/fib/filelisting"
 	"net/http"
 	"os"
 )
 
+type appHandler func(writer http.ResponseWriter, request *http.Request) error
+
+//返回参数类型
+func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
+	//这是返回函数本身
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer, http.StatusText(code), code)
+		}
+	}
+}
 func main() {
 	//http.HandleFunc("list/errhandling/filelistingserver/web.go ")
-	http.HandleFunc("/list/", func(
-		writer http.ResponseWriter, request *http.Request) {
-		path := request.URL.Path[len("/list/"):]
-		file, err := os.Open(path)
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-
-			return
-		}
-		defer file.Close()
-		all, err := ioutil.ReadAll(file)
-		if err != nil {
-			panic(err)
-		}
-		writer.Write(all)
-	})
-	err := http.ListenAndServe(":8888", nil)
-	if err != nil {
-		panic(err)
-	}
+	http.HandleFunc("/list/", errWrapper(filelisting.HandlerFileList))
 }
---
+++ functional/fib/filelisting/handler.go
@@ -0,0 +1,27 @@
+package filelisting
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func HandlerFileList(writer http.ResponseWriter, request *http.Request) error {
+	path := request.URL.Path[len("/list/"):]
+	file, err := os.Open(path)
+	if err != nil {
+		//http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	defer file.Close()
+	all, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	writer.Write(all)
+	err = http.ListenAndServe(":8888", nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
