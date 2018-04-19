39ebca9be57eab999e521c3e2ee0ffc7039609f4
xiangbaoyan <271190187@qq.com>
2018-04-19 09:53:43 +0800

    Merge branch 'master' of https://github.com/xiangbaoyan/study_go_test
    # Conflicts:
    #	.gitignore

---
+++ pipeline/nodes.go
@@ -0,0 +1,40 @@
+package pipeline
+
+import (
+	"fmt"
+	"sort"
+)
+
+func ArraySource(a ...int) <-chan int {
+	ch := make(chan int)
+	go func() {
+
+		for _, v := range a {
+			fmt.Printf("take data %d:\n", v)
+			//第一版在这 就停了，ch 关闭了
+			ch <- v
+		}
+		//close第二版放在这就成功了，原因：放下边关得太快了
+		close(ch)
+
+	}()
+
+	//	close(ch) 第一版加到这，上面就close了
+	return ch
+}
+
+func InMemorySort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		a := []int{}
+		for v := range in {
+			a = append(a, v)
+		}
+		sort.Ints(a)
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
---
+++ pipelinedemo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xiangbaoyan/study_go_test/pipeline"
+	"time"
+)
+
+func main() {
+	p := pipeline.InMemorySort(pipeline.ArraySource(5, 3, 3, 2, 1, 9, 22, 343))
+	for v := range p {
+		fmt.Println(v)
+	}
+
+	time.Sleep(time.Second)
+}
